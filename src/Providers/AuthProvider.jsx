5410f32ee19da709e71b443f861e75a0536df80d
rakib8680 <rakib.cst.1st@gmail.com>
2023-06-07 17:34:19 +0600

    aut context added

---
+++ src/Providers/AuthProvider.jsx
@@ -0,0 +1,91 @@
+import { createContext, useEffect, useState } from "react";
+import { createUserWithEmailAndPassword, getAuth,  GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
+import app from "../firebase.config";
+
+
+
+
+export const AuthContext = createContext(null)
+const auth = getAuth(app);
+
+
+
+
+
+
+const AuthProvider = ({ children }) => {
+
+    const [user, setUser] = useState(null)
+    const [loading, setLoading] = useState(true);
+
+
+
+
+    // google signIn 
+    const googleProvider = new GoogleAuthProvider()
+    const handleGoogleSignIn = () => {
+        setLoading(true)
+        return signInWithPopup(auth, googleProvider)
+    }
+
+
+
+
+    // // register with email and pass 
+    const registerUser = (email, password) => {
+        setLoading(true);
+        console.log(loading);
+        return createUserWithEmailAndPassword(auth, email, password)
+    };
+
+
+    // // sign in with email and pass
+    const signInUser = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password)
+    };
+
+
+    // // update profile 
+    const updateUser = (user, name, photo) => {
+        return updateProfile(user, {
+            displayName: name,
+            photoURL: photo
+        })
+    }
+
+    // // sign out 
+    const logOut = () => {
+        setLoading(true);
+        return signOut(auth)
+    }
+
+    // set current user 
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser)
+            setLoading(false)
+        })
+        return () => unsubscribe()
+    }, []);
+
+
+    // auth information 
+    const authInfo = {
+        user,
+        handleGoogleSignIn,
+        logOut,
+        registerUser,
+        signInUser,
+        loading,
+        updateUser,
+    }
+
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,14 @@ import { RouterProvider } from 'react-router-dom'
 import { router } from './routes/Routes'
 import './index.css'
 import { Toaster } from 'react-hot-toast'
+import AuthProvider from './Providers/AuthProvider'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router} >
-    <Toaster />
-  </RouterProvider>
+  <div>
+    <AuthProvider>
+      <Toaster />
+      <RouterProvider router={router} />
+    </AuthProvider>
+  </div>
+
 )
